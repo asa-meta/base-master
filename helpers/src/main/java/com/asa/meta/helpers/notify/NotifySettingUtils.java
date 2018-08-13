d3b5b18f3d15c9d5e70ecb4ecf47422f6a358c80
AsaInZhuHai <475348530@qq.com>
2018-08-13 14:43:10 +0800

    Initial commit
    Signed-off-by: AsaInZhuHai <475348530@qq.com>

--- helpers/src/main/java/com/asa/meta/helpers/notify/NotifySettingUtils.java
+++ helpers/src/main/java/com/asa/meta/helpers/notify/NotifySettingUtils.java
@@ -1,5 +1,6 @@
 package com.asa.meta.helpers.notify;
 
+import android.annotation.SuppressLint;
 import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Context;
@@ -11,6 +12,8 @@ import android.support.v4.app.NotificationManagerCompat;
 
 import com.asa.meta.helpers.androidOs.OSRomUtils;
 
+import java.lang.reflect.Method;
+
 public class NotifySettingUtils {
 
     //  是否有通知的权限
@@ -19,6 +22,76 @@ public class NotifySettingUtils {
             return NotificationManagerCompat.from(context).areNotificationsEnabled();
         } else {
             return true;
+        }
+
+    }
+
+
+    public static void collapsingNotification(Context context) {
+        @SuppressLint("WrongConstant") Object service = context.getSystemService("statusbar");
+
+        if (null == service) {
+            return;
+        }
+
+        try {
+
+            Class<?> clazz = Class.forName("android.app.StatusBarManager");
+            int sdkVersion = android.os.Build.VERSION.SDK_INT;
+            Method collapse = null;
+
+            if (sdkVersion <= 16) {
+                collapse = clazz.getMethod("collapse");
+            } else {
+                collapse = clazz.getMethod("collapsePanels");
+            }
+
+            collapse.setAccessible(true);
+            collapse.invoke(service);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+    public static void expandNotification(Context context) {
+        @SuppressLint("WrongConstant")
+        Object service = context.getSystemService("statusbar");
+
+        if (null == service) {
+            return;
+        }
+
+        try {
+            Class<?> clazz = Class.forName("android.app.StatusBarManager");
+            int sdkVersion = android.os.Build.VERSION.SDK_INT;
+            Method expand = null;
+            if (sdkVersion <= 16) {
+                expand = clazz.getMethod("expand");
+            } else {
+                /*
+
+                 * Android SDK 16之后的版本展开通知栏有两个接口可以处理
+
+                 * expandNotificationsPanel()
+
+                 * expandSettingsPanel()
+
+                 */
+
+                //expand =clazz.getMethod("expandNotificationsPanel");
+                expand = clazz.getMethod("expandSettingsPanel");
+
+            }
+
+            expand.setAccessible(true);
+            expand.invoke(service);
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
     }
